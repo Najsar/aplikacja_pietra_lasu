b4349d4cff2927489db77b6c5383f1c298be54fa
Najsar <pat.najsarek@gmail.com>
2019-11-25 10:23:30 +0100

    init

---
+++ main.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+require('electron-reload')(__dirname);
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 540,
+    height: 960,
+    frame: false,
+    icon: 'build/icon.png',
+    fullscreen: true
+  })
+
+  mainWindow.removeMenu()
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
+
+app.on('browser-window-created',function(e,window) {
+  window.removeMenu();
+});
